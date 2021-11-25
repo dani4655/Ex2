8bc9dd394f5bb5d5f6d8148e4b44e1b8806b562e
Segev955 <segev95@gmail.com>
2021-11-25 19:50:09 +0200

    more

--- src/api/NodeDataImpl.java
+++ src/api/NodeDataImpl.java
@@ -1,17 +1,30 @@
 package api;
 
 public class NodeDataImpl implements NodeData {
-    int node_id;
     GeoLocation geoLocation;
+    int key; // nodeID
+    int tag;  // color
+    double node_weight;
+    String remark; // meta data
+
+    public NodeDataImpl(GeoLocation geoLocation, int node_id, int tag, double node_weight, String remark) {
+        this.geoLocation = geoLocation;
+        this.key = node_id;
+        this.tag = tag;
+        this.node_weight = node_weight;
+        this.remark = remark;
+    }
 
     @Override
     public int getKey() {
-        return this.node_id;
+        return this.key;
     }
 
     @Override
     public GeoLocation getLocation() {
-        return geoLocation;
+        if (this.geoLocation == null)
+            return null;
+        return this.geoLocation;
     }
 
     @Override
@@ -21,31 +34,31 @@ public class NodeDataImpl implements NodeData {
 
     @Override
     public double getWeight() {
-        return 0;
+        return node_weight;
     }
 
     @Override
     public void setWeight(double w) {
-
+        this.node_weight = w;
     }
 
     @Override
     public String getInfo() {
-        return null;
+        return this.remark;
     }
 
     @Override
     public void setInfo(String s) {
-
+        this.remark = s;
     }
 
     @Override
     public int getTag() {
-        return 0;
+        return this.tag;
     }
 
     @Override
     public void setTag(int t) {
-
+        this.tag = t;
     }
 }
