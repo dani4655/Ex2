eed1007157ee6e4c222d949d0d4dc4a1c0102a66
Segev955 <segev95@gmail.com>
2021-12-02 19:24:01 +0200

    isConnected

--- src/api/DirectedWeightedGraphAlgorithmsImpl.java
+++ src/api/DirectedWeightedGraphAlgorithmsImpl.java
@@ -1,9 +1,11 @@
 package api;
 
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.LinkedList;
-import java.util.List;
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.*;
+
+import com.google.gson.*;
 
 public class DirectedWeightedGraphAlgorithmsImpl implements DirectedWeightedGraphAlgorithms {
     private DirectedWeightedGraph graph;
@@ -29,16 +31,16 @@ public class DirectedWeightedGraphAlgorithmsImpl implements DirectedWeightedGrap
         DirectedWeightedGraph nDWG = new DirectedWeightedGraphImpl();
         while (nodeDataIterator.hasNext()) {
             NodeDataImpl node = (NodeDataImpl) nodeDataIterator.next();
-            node= copy(node.getKey());
+            node = copy(node.getKey());
             nDWG.addNode(node);
         }
         while (edgeDataIterator.hasNext()) {
             EdgeData edgei = edgeDataIterator.next();
-            int src= edgei.getSrc(), dst= edgei.getDest();
+            int src = edgei.getSrc(), dst = edgei.getDest();
             double weight = edgei.getWeight();
-            nDWG.connect(src,dst,weight);
-            nDWG.getEdge(src,dst).setInfo(edgei.getInfo());
-            nDWG.getEdge(src,dst).setTag(edgei.getTag());
+            nDWG.connect(src, dst, weight);
+            nDWG.getEdge(src, dst).setInfo(edgei.getInfo());
+            nDWG.getEdge(src, dst).setTag(edgei.getTag());
         }
         return nDWG;
     }
@@ -58,23 +60,25 @@ public class DirectedWeightedGraphAlgorithmsImpl implements DirectedWeightedGrap
         LinkedList<Integer> queue = new LinkedList<Integer>();
         int v = graph.nodeIter().next().getKey();
         int color[] = new int[num]; //0 = white, 1 = gray, 2 = black
-            if (color[v] == 0) {
-                color[v] = 1; //gray
-                queue.add(v);
-                BFS(queue, color);
-        }
+        color[v] = 1; //gray
+        queue.add(v);
+        BFS(queue, color, this.graph);
+
         for (int i = 0; i < num; i++) {
             if (color[i] == 0)
                 return false;
         }
+        if (!transpose())
+            return false;
+
         return true;
     }
 
-    private boolean BFS(LinkedList<Integer> queue, int[] color) {
+    private boolean BFS(LinkedList<Integer> queue, int[] color, DirectedWeightedGraph g) {
         if (queue.isEmpty())
             return true;
         int v = queue.poll();
-        Iterator<EdgeData> ei = graph.edgeIter(v);
+        Iterator<EdgeData> ei = g.edgeIter(v);
         while (ei.hasNext()) {
             int dest = ei.next().getDest();
             if (color[dest] == 0) {
@@ -83,7 +87,32 @@ public class DirectedWeightedGraphAlgorithmsImpl implements DirectedWeightedGrap
             }
         }
         color[v] = 3; //black
-        return BFS(queue, color);
+        return BFS(queue, color, this.graph);
+    }
+
+    private boolean transpose() {
+        Iterator<EdgeData> ei = graph.edgeIter();
+        Iterator<NodeData> nd = graph.nodeIter();
+        DirectedWeightedGraph g = new DirectedWeightedGraphImpl();
+        while (nd.hasNext()) { //new graph
+            NodeData n = nd.next();
+            g.addNode(n);
+        }
+        while (ei.hasNext()) { //transpose edges (connect)
+            EdgeData e = ei.next();
+            g.connect(e.getDest(), e.getSrc(), e.getWeight());
+        }
+        LinkedList<Integer> q = new LinkedList<Integer>();
+        int v = g.nodeIter().next().getKey();
+        int color[] = new int[g.nodeSize()]; //0 = white, 1 = gray, 2 = black
+        color[v] = 1; //gray
+        q.add(v);
+        BFS(q, color, g);
+        for (int i = 0; i < g.nodeSize(); i++) {
+            if (color[i] == 0)
+                return false;
+        }
+        return true;
     }
 
     @Override
@@ -113,6 +142,17 @@ public class DirectedWeightedGraphAlgorithmsImpl implements DirectedWeightedGrap
 
     @Override
     public boolean load(String file) {
+        try {
+//            String s = "{'src':'w':'dest'}";
+            Gson gson = new Gson();
+//            DirectedWeightedGraph gra;
+            Reader reader = Files.newBufferedReader(Paths.get("data/G1.json"));
+            DirectedWeightedGraphAlgorithms gra = gson.fromJson(reader, DirectedWeightedGraphAlgorithms.class);
+            System.out.println(gra.toString());
+            reader.close();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
         return false;
     }
 }
